0318c28f382084b2c585736f14b0a35a474454b1
julellisg <julianellisg@gmail.com>
2023-02-23 18:35:29 -0500

    website with completed information
    - still far from done; very early stages of design

---
+++ github_cards.js
@@ -0,0 +1,28 @@
+function displayGitHubRepos(username) {
+  const API_URL = `https://api.github.com/users/${username}/repos`;
+
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+      const reposList = document.getElementById('repos-list');
+      let html = '';
+      data.filter(repo => repo.topics.includes("project")).forEach(repo => {
+        // 
+        // ${repo.html_url}
+        // ${repo.name} 
+        // ${repo.description || ''}
+        // 
+        html += `
+          <div class="repo-container">
+            <h2>${repo.name}</h2>
+            <p>${repo.description}</p>
+            <a href="${repo.html_url}" class="btn">View on GitHub</a>
+          </div>
+        `;
+      });
+      reposList.innerHTML = html;
+    })
+    .catch(error => console.error(error));
+}
+
+displayGitHubRepos("Julellisg");  
